2bb75ba4ea212bdd09d275c8ff732a96266a129c
AlluetV <alluetvalenzuela@gmail.com>
2022-05-09 16:43:09 -0500

    fixed html administration

--- TravelJS/src/app/modules/administrator/administrator.component.ts
+++ TravelJS/src/app/modules/administrator/administrator.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AdministratorService } from 'src/app/administrator.service';
+import { Destination } from 'src/app/destination';
+import { PlanetsService } from 'src/app/planets.service';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-administrator',
@@ -6,10 +14,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrator.component.css']
 })
 export class AdministratorComponent implements OnInit {
+  
+  //get 
+  planets$!:Observable<Destination[]>;
+ 
+  //post
+  addedDestination! : Destination;
 
-  constructor() { }
+  confirmation! : Destination;
 
-  ngOnInit(): void {
+
+
+  constructor( private route : ActivatedRoute, private planetService: PlanetsService, private administratorService: AdministratorService, private formBuilder:FormBuilder) {}
+
+  ngOnInit(){
+  //get
+    this.planets$ = this.planetService.getPlanetDetails();
+    console.log(this.planets$);
+  //post
+     this.addedDestination = {id:this.addedDestination.id , name: this.addedDestination.name, description: this.addedDestination.description, url: this.addedDestination.url};
+  }
+
+  onSubmitAdmin(){
+    console.log("on submit");
+    console.log(this.addedDestination);
+    this.administratorService.addedDestination(this.addedDestination);
   }
 
 }
